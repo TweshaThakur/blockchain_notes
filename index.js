dbc82941f4fcfc118e143c697b4bf0de7ac69143
TweshaThakur <tweshathakur@gmail.com>
2025-02-24 14:43:35 +0530

    added visit counter

---
+++ index.js
@@ -0,0 +1,15 @@
+var counterContainer = document.getElementById("sfce2jydsp2q9sra3yt7m3zk7q4hue5bguh");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+
